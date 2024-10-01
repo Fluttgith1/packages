d02bfcb804f8093db3a43be75ece513e8e69ee24
Alexander Troshkov <20989940+aednlaxer@users.noreply.github.com>
2024-10-01 09:53:36 +0300

    Fix draggable flag

--- packages/google_maps_flutter/google_maps_flutter_web/lib/src/convert.dart
+++ packages/google_maps_flutter/google_maps_flutter_web/lib/src/convert.dart
@@ -618,7 +618,8 @@ Future<O> _markerOptionsFromMarker<T, O>(
             marker.position.longitude,
           )
           ..title = sanitizeHtml(marker.infoWindow.title ?? '')
-          ..zIndex = marker.zIndex;
+          ..zIndex = marker.zIndex
+          ..gmpDraggable = marker.draggable;
     return options as O;
   } else {
     final gmaps.MarkerOptions options = gmaps.MarkerOptions()
